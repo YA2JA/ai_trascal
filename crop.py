f5518b982da884e6cc6e3a83721c95a6b80f3398
Jakob Oganesyan <jakoganesyan@gmail.com>
2023-07-31 08:20:44 +0200

    update

---
+++ crop.py
@@ -0,0 +1,8 @@
+import trainer.tools as tools
+
+tools.INPUT_DIR = r"C:\Users\jakog\Documents\kek\ai_trascal\input"
+tools.OUTPUT_DIR = r"C:\Users\jakog\Documents\kek\ai_trascal\output"
+tools.CLASS_LIST = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '.', 'L']
+
+lab = tools.Label()
+lab.run()
---
+++ main.py
@@ -0,0 +1,5 @@
+import trainer.tools as tools
+
+if __name__ == "__main__":
+    lab = tools.Train()
+    lab.run()
